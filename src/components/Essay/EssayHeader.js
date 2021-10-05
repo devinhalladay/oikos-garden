de6775fe02c600131a3bf40f0aa3e1f9a71908da
Devin Halladay <devin@devinhalladay.com>
2021-10-04 21:00:14 -0400

    Nav and essay type tweaks

--- src/components/Essay/EssayHeader.js
+++ src/components/Essay/EssayHeader.js
@@ -61,26 +61,28 @@ function EssayHeader({ post, isLinked, mini }) {
     );
   } else {
     return (
-      <header className="mb-10 mt-20">
-        <div className="mb-1 text-gray-500 font-sans lining-nums">
-          <small className=" mr-3">{post.frontmatter.date}</small>
-          <small>{post.fields.readingTime.text}</small>
+      <header className="pb-14 mb-14 border-gray-100 border-b-8">
+        <div className="mb-1 text-gray-500 font-sans lining-nums text-center font-medium">
+          <small className="mr-3">{post.frontmatter.date}</small>
+          <small className="mr-3">
+            {post.fields.readingTime.text}
+          </small>
+          {post.frontmatter.tags &&
+            post.frontmatter.tags.map((tag, i) => (
+              <TagLink tag={tag} size={'sm'} />
+            ))}
         </div>
-        <div className="mb-2">
-          <h1 className="mb-2">{post.frontmatter.title}</h1>
+        <div className="mb-2 text-center justify-center items-center flex flex-col">
+          <h1 className="mb-2 font-serif font-light pb-8">
+            {post.frontmatter.title}
+          </h1>
+          <div className="bg-domain-red w-px h-64"></div>
           {post.frontmatter.subtitle && (
-            <h2 className="font-sans text-2xl indent-0 text-gray-500 mt-0">
+            <p className="font-sans text-2xl indent-0 mt-8">
               {post.frontmatter.subtitle}
-            </h2>
+            </p>
           )}
         </div>
-        {post.frontmatter.tags && (
-          <div className="font-sans">
-            {post.frontmatter.tags.map((tag, i) => (
-              <TagLink tag={tag} size={mini ? 'sm' : 'lg'} />
-            ))}
-          </div>
-        )}
       </header>
     );
   }
